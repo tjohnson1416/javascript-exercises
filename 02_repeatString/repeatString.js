73e5dbd57248582b4356ee8c851889fd08760ec7
Trevor Johnson <johnson.trevor.kent@gmail.com>
2023-09-10 17:03:47 -0600

    Finised the first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    if (num >= 0) {
+        let repeatedString = string.repeat(num);
+        return repeatedString;
+    }
+    else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
