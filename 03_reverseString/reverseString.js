0bf916abf216d8238bd43b8408c61cb60753c0be
Trevor Johnson <johnson.trevor.kent@gmail.com>
2023-09-10 17:27:24 -0600

    Finished the third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let holdingArray = stringToReverse.split('');
+    holdingArray = holdingArray.reverse();
+    let reverseString = '';
+    for (let members in holdingArray) {
+        reverseString += holdingArray[members];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
