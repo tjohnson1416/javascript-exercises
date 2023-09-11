7f31d15e2aebc885e682366099073368b5dc675a
Trevor Johnson <johnson.trevor.kent@gmail.com>
2023-09-11 17:24:32 -0600

    Finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...removedValues) {
+    removedValues.forEach((removeItem) => {
+        let removeIndex = inputArray.indexOf(removeItem);
+        /*indexOf returns a -1 if the value is not found*/
+        if (removeIndex > -1){
+            inputArray.splice(removeIndex,1);
+        }
+    })
+    return inputArray;
 };
 
 // Do not edit below this line
