9a3d803b9807e91ed610cfe88242afc0c8874296
Trevor Johnson <johnson.trevor.kent@gmail.com>
2023-09-11 17:53:38 -0600

    Adjusted sumAll() to handle when the large number is first in the argument

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,16 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
-    while (num1 <= num2){
-        sum += num1;
-        num1++;
+    if (num1 < num2){
+        while (num1 <= num2){
+            sum += num1;
+            num1++;
+        }
+    }
+    else {
+        while (num2 <= num1){
+            sum += num2;
+            num2++;
+        } 
     }
     return sum;
 };
