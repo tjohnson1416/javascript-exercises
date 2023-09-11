e1d2c5c21572cbececd278e1a11da649f8941abb
Trevor Johnson <johnson.trevor.kent@gmail.com>
2023-09-11 17:33:31 -0600

    sumAll() function is able to pass the first two tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    while (num1 <= num2){
+        sum += num1;
+        num1++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
