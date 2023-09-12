ebda65e01fa439918cef18dd7739e82a62870be8
Trevor Johnson <johnson.trevor.kent@gmail.com>
2023-09-11 21:53:14 -0600

    Finished the fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,23 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
-    if (num1 < num2){
-        while (num1 <= num2){
-            sum += num1;
-            num1++;
-        }
+    if (num1 < 0 || num2 < 0 || typeof num1 != 'number' || typeof num2 != 'number') {
+        return 'ERROR'
     }
     else {
-        while (num2 <= num1){
-            sum += num2;
-            num2++;
-        } 
+        if (num1 < num2){
+            while (num1 <= num2){
+                sum += num1;
+                num1++;
+            }
+        }
+        else {
+            while (num2 <= num1){
+                sum += num2;
+                num2++;
+            } 
+        }
+        return sum;
     }
-    return sum;
 };
 
 // Do not edit below this line
