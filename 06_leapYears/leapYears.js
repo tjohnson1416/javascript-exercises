9fd32fd7844ea6993d9949e0afe4a79d0ac53a27
Trevor Johnson <johnson.trevor.kent@gmail.com>
2023-09-11 22:05:21 -0600

    Finished the sixth exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    if (((inputYear % 4) == 0) && ((inputYear % 100 != 0) || (inputYear % 400 == 0))){
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
