b8006b2945692cf7285a948125030bd906fbfe9f
Trevor Johnson <johnson.trevor.kent@gmail.com>
2023-09-23 14:19:27 -0600

    Finished exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum = array.reduce((runningSum, a) => runningSum + a, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const result = array.reduce((runningTotal,a) => runningTotal * a,1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+	for(let i = 1; i <= a; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
