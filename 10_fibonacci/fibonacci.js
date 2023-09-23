c2bf740c10964a47eb911181c3184ff9d068c71c
Trevor Johnson <johnson.trevor.kent@gmail.com>
2023-09-23 17:00:52 -0600

    Finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return 'OOPS'
+    }
+    return n < 1 ? 1
+         : n <= 2 ? 1
+         : fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
