5cf201cb1de18a425f6963a8e867c459f991c819
Trevor Johnson <johnson.trevor.kent@gmail.com>
2023-09-23 15:50:00 -0600

    Finished exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    const lowerCase = input.toLowerCase();
+    const noSpaces = lowerCase.replace(/\s/g, '').replace(/[^\w\s\']|_/g, "");
+    const len = noSpaces.length;
+    for (let i = 0; i < (len/2); i++){
+        if (noSpaces.charAt(i) !== noSpaces.charAt(len - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
