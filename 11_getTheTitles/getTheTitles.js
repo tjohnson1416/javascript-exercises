f3a650bba2641b94c2621360c9336cf7ff9ef527
Trevor Johnson <johnson.trevor.kent@gmail.com>
2023-09-24 18:52:36 -0600

    Finished the 11th exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    const bookTitles = obj.map(books => books.title);
+    return bookTitles;
 };
 
 // Do not edit below this line
